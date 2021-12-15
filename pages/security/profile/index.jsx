6359915925f97591f51fc142694c9a29d2a47a70
CesarSuarezWD <cesarsurezmedina@gmail.com>
2021-12-15 16:35:37 -0500

    Se modifica el archivo pages/security/profile/index.jsx, para que oculte el navbar y los demas elemntos cuando el usuario no este logueado, y lo despliegue cuando ya se loguee

--- pages/security/profile/index.jsx
+++ pages/security/profile/index.jsx
@@ -6,38 +6,52 @@ import StreakAndRank from '../../../components/challenges/StreakAndRank/StreakAn
 import Achievements from '../../../components/challenges/achievements/Achievements'
 import Footer from '../../../components/challenges/footer/Footer'
 import styles from '../../../styles/Profile.module.scss';
+import { useUser } from '@auth0/nextjs-auth0';
 
 
 async function getStaticProps(context) {
   const res = await fetch(`http:localhost:3500/data`)
   const data = await res.json()
-
+  
   if (!data) {
     return {
       notFound: true,
     }
   }
-
+  
   return {
     props: { data },
   }
 }
 
 const Profile = (data) => {
-console.log(data)
-
+  console.log(data)
+  const {user} = useUser();
+  
   if (data) {
+    if(user)
+    {
+      return (
+        <div className={styles.container}>
+            <Header />
+            <Navbar  />
+            <StreakAndRank ranks={data.ranks} challenges={data.challenges} />
+            <Usersystem  data={data} ranks={data.ranks} />
+            <Achievements  goals={data.achievements} />
+           {/*  <UserGraph  activity={data.activity} /> */}
+            <Footer />
+        </div>
+      );
+
+    }
+
     return (
       <div className={styles.container}>
           <Header />
-          <Navbar  />
-          <StreakAndRank ranks={data.ranks} challenges={data.challenges} />
-          <Usersystem  data={data} ranks={data.ranks} />
-          <Achievements  goals={data.achievements} />
-         {/*  <UserGraph  activity={data.activity} /> */}
           <Footer />
       </div>
     );
+
   } else {
     return null;
    }
