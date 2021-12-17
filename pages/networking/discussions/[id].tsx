d5e261be20cb427c9cd277aef16a67ffe1a8c337
Vanessa Alonso <vpalonsog@gmail.com>
2021-12-16 21:35:31 -0500

    Add skeleton loader to discussion view

--- pages/networking/discussions/[id].tsx
+++ pages/networking/discussions/[id].tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
-
 import { useRouter } from 'next/router';
-
 import { Grid } from '@mui/material';
-
 import { DiscussionPost } from '../../../components/networking/DiscussionPost';
 import { DiscussionBreadCrumb } from '../../../components/networking/DiscussionBreadCrumb';
 import { makeStyles } from '@mui/styles';
 import { DiscussionLikeCounter } from '../../../components/networking/DiscussionLikeCounter';
 import { Box } from '@mui/system';
+import { Skeleton } from '@mui/material';
 
 const useStyle = makeStyles({
     likeButtonContainer: {
         paddingTop: '16px'
+    },
+    Discussion_bread: {
+        marginTop: '12px'
+    },
+    Discussion_post: {
+        marginTop: '30px'
     }
 })
 
@@ -29,47 +33,69 @@ function Discussion() {
         is_active: false
     });
 
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+
         if (id) {
             fetch(`https://get-hired-forum-dev.herokuapp.com/api/discussions/${id}`)
                 .then(response => response.json())
                 .then(discussion => {
                     setDiscussionData(discussion);
+                    setLoading(false)
                 });
         }
     }, [id])
+
 
     return (
         <>
             <Grid container justifyContent='center'>
                 <Grid item sm={11} md={7} lg={8} xl={6}>
-                    <DiscussionBreadCrumb />
+                    {loading
+                        ?
+                        <div className={classes.Discussion_bread}>
+                            <Skeleton variant='text' width={180} height={30} />
+                        </div>
+                        :
+                        <DiscussionBreadCrumb />
+
+                    }
                 </ Grid>
             </Grid>
             <Grid container justifyContent='center'>
-                <Grid item lg={9} xl={6}>
-                    {discussionData.content &&
-                        <Box display="flex" flexDirection="row">
-                            <DiscussionLikeCounter
-                                isActive={discussionData.is_active}
-                                boxProps={{
-                                    pt: '1em'
-                                }}
-                            />
-                            
-                            <DiscussionPost
-                                title={discussionData.title}
-                                content={discussionData.content}
-                                created_at={discussionData.created_at.slice(0, 10)}
-                                created_by="Cesar Turner"
-                            />
-                        </Box>
-                    }
-                </Grid>
+                {
+                    loading
+                        ?
+                        <div className={classes.Discussion_post}>
+                            <Skeleton variant='rectangular' width={900} height={500} />
+                        </div>
+                        :
+                        <Grid item lg={9} xl={6}>
+                            {discussionData.content &&
+                                <Box display="flex" flexDirection="row">
+                                    <DiscussionLikeCounter
+                                        isActive={discussionData.is_active}
+                                        boxProps={{
+                                            pt: '1em'
+                                        }}
+                                    />
+
+                                    <DiscussionPost
+                                        title={discussionData.title}
+                                        content={discussionData.content}
+                                        created_at={discussionData.created_at.slice(0, 10)}
+                                        created_by={discussionData.created_by}
+                                    />
+                                </Box>
+                            }
+                        </Grid>
+
+                }
             </Grid>
         </>
     );
 }
+
 
 export default Discussion;
