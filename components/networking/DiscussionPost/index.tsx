8dc8e9a26155e672fd23476dcb2f3d9d2a2e901a
CesarSuarezWD <cesarsuarezmedina@gmail.com>
2022-02-11 16:40:44 -0500

    Security/feat: creation of report button and modal to reports any forum topic the breaks the comunity rules

--- components/networking/DiscussionPost/index.tsx
+++ components/networking/DiscussionPost/index.tsx
@@ -3,6 +3,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import { makeStyles } from '@mui/styles';
+import { ReportButton } from '../../security/ReportButton/index';
 
 const useStyle = makeStyles({
     userIcon: {
@@ -47,16 +48,21 @@ function DiscussionPost(props: DiscussionProps) {
                     ))}
                 </Box>
             </Box>
-            <Box pl={1.5} sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Box mr={95} pl={1.5} sx={{ display: 'flex', alignItems: 'center' }}>
 
-                <AccountCircleTwoToneIcon className={classes.userIcon} fontSize='large' />
+                    <AccountCircleTwoToneIcon className={classes.userIcon} fontSize='large' />
 
-                <Typography paragraph m={0} px={1}>
-                    {props.created_by}
-                </ Typography>
-                <Typography paragraph m={0}>
-                    {props.created_at}
-                </ Typography>
+                    <Typography paragraph m={0} px={1}>
+                        {props.created_by}
+                    </ Typography>
+                    <Typography paragraph m={0}>
+                        {props.created_at}
+                    </ Typography>
+                </Box>  
+                <Box pr={1.5} sx={{ display: 'flex', alignContent: 'center'}}>
+                    <ReportButton />                    
+                </Box>              
             </Box>
         </div>
 
