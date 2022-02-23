438bea262aaacdcd49a2acc496fd81f5472a0d90
caprun <26richardr@gmail.com>
2022-02-23 13:15:36 -0600

    feat: Code Runner Connected with backend side
    Now code runner and categories page is connected with the backend

--- components/challenges/playground/codeEditor/codeEditor.jsx
+++ components/challenges/playground/codeEditor/codeEditor.jsx
@@ -1,94 +1,49 @@
-import React, { useState, useRef } from 'react';
-import Coolicon from '../../../../public/icons/coolicon.svg';
+import React, { useRef } from 'react';
 import Editor from '@monaco-editor/react';
-import Image from 'next/image';
 import styled from '@emotion/styled';
 
-const ItemCodeView = styled.div`
-  box-shadow: 3px 3px 3px #A779FF;
-  grid-area: codeView;
-  & section {
-    margin: 0;
+function CodeEditor() {
+  const editorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
   }
-`;
-
-const ItemButton = styled.div`
-  display: flex;
-  width: 100%;
-  height: 8%;
-  justify-content: end;
-  align-items: center;
-`;
-
-const ItemButtonRun = styled.div`
-  height: 35px;
-  width: 131px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  color: #A779FF;
-  background-color: white;
-  border-radius: 6px;
-  border: 1px solid #A779FF;
-  font-size: 14px;
-  font-weight: bold;
-  padding-top: 3px;
-  &:hover {
-    color: white;
-    background-color: #A779FF;
-    border: 0.5px solid rgba(85, 91, 255, 0.79);
+  function showValue() {
+    alert(editorRef.current.getValue());
   }
-`;
-
-const ItemButtonSubmit = styled.div`
-  height: 35px;
-  width: 82px;
-  font-size: 14px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  color: white;
-  font-weight: bold;
-  background-color: #A779FF;
-  border-radius: 6px;
-  border: 1px solid #A779FF;
-  padding-top: 3px;
-  margin: 0 30px;
-  &:hover {
-    color: #A779FF;
+  const ItemButtonRun = styled.div`
+    z-index: 1;
+    height: 35px;
+    width: 131px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: #a779ff;
     background-color: white;
-    border: 0.5px solid rgba(85, 91, 255, 0.79);
-  }
-`;
-
-function CodeEditor () {
-  const [ content, setContent ] = useState('// Comienza tu codigo aqui...')
-
-  const editorRef = useRef(null)
-
-  const handleEditor = (editor, monaco) => {
-    editorRef.current = editor
-  }
+    border-radius: 6px;
+    border: 1px solid #a779ff;
+    font-size: 14px;
+    font-weight: bold;
+    padding-top: 3px;
+    &:hover {
+      color: white;
+      background-color: #a779ff;
+      border: 0.5px solid rgba(85, 91, 255, 0.79);
+    }
+  `;
 
   return (
-    <ItemCodeView>
-        <Editor
-        height='700px'
-        defaultLanguage='javascript'
-        defaultValue={content}
-        theme='vs-dark'
-        onChange={(value) => setContent(value)}
-        onMount={handleEditor}
+    <React.Fragment>
+      <Editor
+        height="90vh"
+        width="100%"
+        defaultLanguage="javascript"
+        defaultValue={''}
+        theme="vs-dark"
       />
-      <ItemButton>
-            <ItemButtonRun onClick={() => alert('Running Challenge ... ')}>
-              Run Tests
-              <Image id='img-icon' src={Coolicon} alt='Run Challenge'/>
-            </ItemButtonRun>
-          <ItemButtonSubmit onClick={() => alert('Sending Challenge ...')}>Submit</ItemButtonSubmit>
-        </ItemButton>
-    </ItemCodeView>
-  )
+      <ItemButtonRun>RUN</ItemButtonRun>
+    </React.Fragment>
+  );
 }
 
-export default CodeEditor
+export default CodeEditor;
