0ab45cfdb380e7cf128eacbcd9f4f78af78291ba
caprun <26richardr@gmail.com>
2022-03-03 18:34:11 -0600

    fix(user): User provider to fix auth issue
    Now user is provided vi Providers/UserProvider
    To retrieve data, use context and the provider.

--- components/security/navbar/Navbar.jsx
+++ components/security/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
 import Avatar from '../../../public/icons/Avatar.png';
 import Forum from '../../../public/icons/forum.svg';
@@ -8,12 +8,12 @@ import styles from '../../../styles/Navbar.module.scss';
 import position from '../../../styles/Profile.module.scss';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0';
+import UserProvider from '../../../Providers/UserProvider';
 
 const NavBar = () => {
 
-  const {user} = useUser();
-
-  if(user) {
+  const user = useContext(UserProvider);
+  if (user) {
 
     return (
       <div className={position.inavbar}>
@@ -21,11 +21,11 @@ const NavBar = () => {
 
           <div className={styles.navBar_user}>
 
-            <Link  href='/' passHref>
+            <Link href='/' passHref>
               <div className={styles.navBar_user_name}>{user.name}</div>
             </Link>
             <div className={styles.avatar_profile}>
-            <Image layout="fixed"  src={Avatar} alt='' />
+              <Image layout='fixed' src={Avatar} alt='' />
             </div>
 
           </div>
@@ -33,35 +33,35 @@ const NavBar = () => {
           <div className={styles.navBar_menu}>
             <ul>
               <li>
-                <Link  href='/networking' passHref>
+                <Link href='/networking' passHref>
                   <div className={styles.navBar_menu_link}>
-                  <Image src={Forum} alt='' /><p className={styles.navBar_menu__text}>Forum</p>
-                    </div>
+                    <Image src={Forum} alt='' /><p className={styles.navBar_menu__text}>Forum</p>
+                  </div>
                 </Link>
               </li>
               <li>
-                <Link  href='/challenges/categories'  passHref>
+                <Link href='/challenges/categories' passHref>
                   <div className={styles.navBar_menu_link}>
                     <Image src={Challenges} alt='' /><p className={styles.navBar_menu__text}>Challenges</p>
-                    </div>
+                  </div>
                 </Link>
               </li>
               <li>
-                <Link  href='/challenges/profile' passHref>
+                <Link href='/challenges/profile' passHref>
                   <div className={styles.navBar_menu_link}>
                     <Image src={Profile} alt='' /><p className={styles.navBar_menu__text}>Profile</p>
-                    </div>
-                  </Link>
+                  </div>
+                </Link>
               </li>
             </ul>
           </div>
 
         </aside>
       </div>
-    )
+    );
 
-  } else {return null}
+  } else {return null;}
 
-}
+};
 
 export default NavBar;
--- pages/security/profile/index.jsx
+++ pages/security/profile/index.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import Header from "../../../components/security/header/Header";
-import Navbar from "../../../components/security/navbar/Navbar";
-import styles from "../../../styles/Landing.module.scss";
-import { CallToAction } from "../../../components/security/CallToAction";
-import JoinToCommunity from "../../../components/security/JoinToCommunity";
-import { useUser } from '@auth0/nextjs-auth0';
+import React, { useContext } from 'react';
+import Header from '../../../components/security/header/Header';
+import Navbar from '../../../components/security/navbar/Navbar';
+import styles from '../../../styles/Landing.module.scss';
+import { CallToAction } from '../../../components/security/CallToAction';
+import JoinToCommunity from '../../../components/security/JoinToCommunity';
+import UserProvider from '../../../Providers/UserProvider';
 
 const LandingPage = () => {
-  const {user} = useUser();
+  const user = useContext(UserProvider);
   if (!user) {
     return (
       <>
-        <Header />
+        <Header user={user} />
         <div className={styles.container}>
-        <JoinToCommunity styles={styles}/>
-        <CallToAction styles={styles}/>
+          <JoinToCommunity styles={styles} />
+          <CallToAction styles={styles} />
         </div>
       </>
     );
   } else {
+    console.log(user);
     return (
       <>
         <Header />
         <Navbar />
         <div className={styles.container}>
-        <JoinToCommunity styles={styles}/>
-        <CallToAction styles={styles}/>
+          <JoinToCommunity styles={styles} />
+          <CallToAction styles={styles} />
         </div>
       </>
-
-    )
+    );
   }
 };
 
